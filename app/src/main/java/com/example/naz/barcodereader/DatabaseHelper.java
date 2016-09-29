9357aec408e1fd791388304af483e4b31891622d
Hrabovkyi <nazar.hrabovskyy@gmail.com>
2016-09-29 21:17:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/naz/barcodereader/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.naz.barcodereader;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Naz on 29.09.2016.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "scan.db";
+    public static final String TABLE_NAME = "Scan_Table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "TXT";
+    public static final String COL_3 = "TYPE";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        //SQLiteDatabase db = this.getReadableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table" + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, TXT TEXT, TYPE TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXIST"+TABLE_NAME);
+        onCreate(db);
+    }
+}
